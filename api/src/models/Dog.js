9bf3ef28b232053f72bc606d48ce7f91cadc5594
Leonardo Bonetto <leonardo1bonetto@protonmail.com>
2022-10-17 13:40:27 -0300

    Finished models

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -7,6 +7,30 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique:true
     },
+    id:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement:true
+    },
+    height:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      validator:{
+        min:0
+      }
+    },
+    weight:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      validator:{
+        min:0
+      }
+    },
+    life_expectancy:{
+      type:DataTypes.INTEGER,
+      allowNull:true
+    }
   });
 };
