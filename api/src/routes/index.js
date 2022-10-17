53a7d53a2baad678d03c4abf06cf98dd85ee82bf
Leonardo Bonetto <leonardo1bonetto@protonmail.com>
2022-10-17 17:52:22 -0300

    Added /dogs to index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
+const {dogs} = require('./dogs')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
 
+router.use('dogs', dogs)
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
